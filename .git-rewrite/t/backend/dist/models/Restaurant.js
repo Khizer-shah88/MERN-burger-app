2e17da0489ea8c3ecb59324a983211cdf868cb68
khizer_shah <khizershah493@gmail.com>
2025-07-25 01:39:24 +0500

    git

--- .git-rewrite/t/backend/dist/models/Restaurant.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-const restaurantSchema = new Schema({
-    _id: { type: String, required: true },
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true },
-});
-export default mongoose.model('Restaurant', restaurantSchema);
--- .git-rewrite/t/backend/dist/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { placeOrder, getAllOrders, updateOrderStatus } from '../controllers/orders.js';
-const router = express.Router();
-router.post('/orders', placeOrder);
-router.get('/orders', getAllOrders);
-router.patch('/orders/:id/status', updateOrderStatus);
-export default router;
